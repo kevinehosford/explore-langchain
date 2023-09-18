76f51ee93c30e091a889e66bb0c827c0e2e50397
Kevin Hosford <kevehosford@gmail.com>
2023-09-18 08:09:45 -0400

    Actually update the query

--- query_md.py
+++ query_md.py
@@ -3,6 +3,8 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 
 vectordb = Chroma(persist_directory='./md_vectors', embedding_function=OpenAIEmbeddings())
 
-query = "summarize"
+query = "How to summarize the average of a field"
 
 docs = vectordb.similarity_search(query, k=5)
+
+print(docs[0].page_content[0:500])
