722dc8fa6a3164b0b5300d90ac19480a9c64406c
Kevin Hosford <kevehosford@gmail.com>
2023-09-26 08:08:38 -0400

    Update some examples. Ignore test data.

--- chat_md.py
+++ chat_md.py
@@ -8,7 +8,7 @@ vectordb = Chroma(persist_directory='./md_vectors', embedding_function=OpenAIEmb
 llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 qa_chain = RetrievalQA.from_chain_type(llm, retriever=vectordb.as_retriever())
 
-question = "How to summarize the average of a field"
+question = "How do you summarize the average of a field"
 
 result = qa_chain({'query': question })
 
